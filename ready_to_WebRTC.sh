6b669128fbf698dfcf78f011d52d7f82606174a0
dnjstjr93 <dnjstjr93@gmail.com>
2022-08-15 17:59:07 -0900

    - 파일 권한 변경

--- ready_to_WebRTC.sh
+++ ready_to_WebRTC.sh
@@ -7,12 +7,6 @@ CHECK_OS="`echo $OS | cut -d" " -f1`"
 
 echo  ${CHECK_OS}
 
-if [ "${OS}" = "Darwin" ]; then
-	/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome -version
-	brew install -cask chromedriver
-	sudo spctl --master-disable 
-else
-	sudo apt-get purge -y chromium-browser
-	sudo apt-get install -y chromium-browser
-	sudo apt-get install -y chromium-chromedriver
-fi
+sudo apt-get purge -y chromium-browser
+sudo apt-get install -y chromium-browser
+sudo apt-get install -y chromium-chromedriver
